53ba8253ee1c6e4049b3538815b783f6f9ae37b4
Nguyen Huu Ngoc <nguyen.huu.ngoc@elife.co.jp>
2018-04-17 18:01:43 +0900

    update

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -83,7 +83,6 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
